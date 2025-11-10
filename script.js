b89cd55892a20a0f8a5ba1516422af292d977270
marwadidolly <marwadidolly20@gmail.com>
2025-11-10 14:41:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const value = btn.textContent;
+
+    if (value === 'C') {
+      display.value = '';
+    } 
+    else if (value === 'DEL') {
+      display.value = display.value.slice(0, -1);
+    } 
+    else if (value === '=') {
+      try {
+        display.value = eval(display.value);
+      } catch {
+        display.value = 'Error';
+      }
+    } 
+    else {
+      display.value += value;
+    }
+  });
+});
